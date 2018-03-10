a8652222dd6749514262bd6e4104a10b2e762bb1
zhaochuanxing <zhaochuanxing@gmail.com>
2018-03-10 15:17:30 +0800

    变更项目目录

---
+++ myapplication/src/main/java/com/xing/myapplication/HelloKotlin.kt
@@ -0,0 +1,13 @@
+package com.xing.myapplication
+
+/**
+ * Created by zhao on 18-3-10.
+ */
+fun sum(a:Int,b:Int):Int{
+    return a+b
+}
+
+fun main(args:Array<String>){
+    print("sum of 3 and 5 is")
+    println(sum(3,5))
+}
---
+++ mylibrary/src/main/java/com/xing/mylibrary/MyKot.kt
@@ -0,0 +1,8 @@
+package com.xing.mylibrary
+
+/**
+ * Created by zhao on 18-3-10.
+ */
+fun main(args:Array<String>){
+    println("sum")
+}
